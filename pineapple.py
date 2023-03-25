897036e33888eca04906ed69630294e08c5922c0
The Trident A.K.A. Xwill <89924712+xwillxu@users.noreply.github.com>
2023-03-25 16:39:52 -0400

    Update pineapple.py

--- pineapple.py
+++ pineapple.py
@@ -1,1 +1,1 @@
-The pineapple is a tropical plant with an edible fruit;
+The pineapple is a tropical plant with an edible fruit;  it is the most economically significant plant in the family Bromeliaceae. 
