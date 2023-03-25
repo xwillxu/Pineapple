fca68bc3b2628b972ea49735af4132c76f4064c5
The Trident A.K.A. Xwill <89924712+xwillxu@users.noreply.github.com>
2023-03-25 16:40:57 -0400

    Update pineapple.py

--- pineapple.py
+++ pineapple.py
@@ -1,1 +1,6 @@
 The pineapple is a tropical plant with an edible fruit;  it is the most economically significant plant in the family Bromeliaceae. 
+The pineapple is indigenous to South America, where it has been cultivated for many centuries.
+
+this.whoLivesUnderAPineapple = "blank"
+
+if(this.whoLivesUnderAPineapple == Spongebob)
