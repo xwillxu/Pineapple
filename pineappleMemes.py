f5aad0e6221f7305dfabd56eca28856f805d7fa9
The Trident A.K.A. Xwill <89924712+xwillxu@users.noreply.github.com>
2023-03-26 14:59:21 -0400

    Update pineappleMemes.py

--- pineappleMemes.py
+++ pineappleMemes.py
@@ -1,8 +1,2 @@
-The pineapple is a tropical plant with an edible fruit;  it is the most economically significant plant in the family Bromeliaceae. 
-The pineapple is indigenous to South America, where it has been cultivated for many centuries.
-
-this.whoLivesUnderAPineapple = "blank"
-this.answerBox = "no answer"
-
-if(this.whoLivesUnderAPineapple == Spongebob):
-  this.answerBox = "correct"
+export default Class PineappleMemes :
+  function 
