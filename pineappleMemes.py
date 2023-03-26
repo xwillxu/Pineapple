c149b11d69555a270cb9c87782cde646b0392163
The Trident A.K.A. Xwill <89924712+xwillxu@users.noreply.github.com>
2023-03-26 15:00:01 -0400

    Update pineappleMemes.py

--- pineappleMemes.py
+++ pineappleMemes.py
@@ -1,2 +1,44 @@
-export default Class PineappleMemes :
-  function 
+class Meme(Window):
+    """Meme"""
+
+    def __init__(self):
+        """Init"""
+
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        pass
+
+    def setup(self):
+        """Setup"""
+
+        arcade.set_background_color(arcade.color.OCEAN_BOAT_BLUE)
+
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """Key Press"""
+
+        pass
+
+    def on_key_release(self, key, modifiers):
+        """Key Press"""
+
+        pass
+
+    def on_draw(self):
+        """Draw"""
+
+        arcade.start_render()
+
+        pass
+
+    def on_update(self, delta_time):
+        """Update"""
+
+        pass
+
+
+if __name__ == "__main__":
+    window = Game()
+    window.setup()
+    arcade.run()
